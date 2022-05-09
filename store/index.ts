05a0d8f54ce6ab9faade5e1581ebc2551a0cfaaa
Ischerba32 <i.scherba32@gmail.com>
2022-05-09 13:29:08 +0300

    added redux store

---
+++ store/index.ts
@@ -0,0 +1,8 @@
+import { Context, createWrapper, MakeStore } from "next-redux-wrapper";
+import { legacy_createStore as createStore } from "redux";
+import { configureStore } from '@reduxjs/toolkit';
+import { reducer, RootState } from "./reducers";
+
+const makeStore: MakeStore<RootState> = (context: Context) => createStore(reducer);
+
+export const wrapper = createWrapper<RootState>(makeStore, {debug: true})
